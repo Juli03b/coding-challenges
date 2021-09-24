9fbb35b87ef844af06d296f872dc2db91108be71
Julio Barrios <julio.junior02@gmail.com>
2021-09-23 22:20:37 -0500

    Add data_structures directory

---
+++ data_structures/singly_linked_list.py
@@ -0,0 +1,7 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    def __repr__(self) -> str:
+        return f"<ListNode val={self.val} next={self.next}>"
